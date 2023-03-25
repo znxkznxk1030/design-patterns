41b83986b828e3f80a4b2b7be07eb72f18b1b87b
arthur kim <znxkznxk1030@gmail.com>
2023-03-25 17:33:56 +0900

    Observer Pattern #1

---
+++ observer/CurrentConditionDisplay.java
@@ -0,0 +1,26 @@
+package observer;
+
+public class CurrentConditionDisplay implements Observer, DisplayElement {
+    private float temperature;
+    private float humidity;
+    private WeatherData weatherData;
+
+    CurrentConditionDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        this.weatherData.registerObserver(this);
+    }
+    
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+        this.temperature = temp;
+        this.humidity = humidity;
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("현재 상태 온도: 온도 " + temperature + "F, 습도 " + humidity + "%");
+    }
+
+    
+}
---
+++ observer/StatisticsDisplay.java
@@ -0,0 +1,29 @@
+package observer;
+
+public class StatisticsDisplay implements Observer, DisplayElement {
+    private WeatherData weatherData;
+
+    private float avgTemperature;
+    private float maxTemperature;
+    private float minTemperature;
+    private int nMeasurements;
+    
+    StatisticsDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        this.nMeasurements = 0;
+        this.weatherData.registerObserver(this);
+    }
+    
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+        this.avgTemperature = ((this.avgTemperature * nMeasurements) + temp) / ++nMeasurements;
+        this.maxTemperature = Math.max(this.maxTemperature, temp);
+        this.minTemperature = Math.min(this.minTemperature, temp);
+        display();
+    }
+    
+    @Override
+    public void display() {
+        System.out.println("평균/ 최고/ 최저 온도 = " + avgTemperature + "/ " + maxTemperature + "/ " + minTemperature);
+    }
+}
